b7408d7e9009ac4ebb8d21adfd0f6320ac5bdf98
Juliano Almada <almadaj@edu.unifor.br>
2023-12-08 14:19:41 -0300

    upload de arquivos pelo adminJs

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,7 +1,10 @@
 import { Category } from "./Category";
 import { Course } from "./Course";
+import { Episode } from "./Episode";
 
 Category.hasMany(Course);
 Course.belongsTo(Category);
+Course.hasMany(Episode);
+Episode.belongsTo(Course);
 
-export { Course, Category };
+export { Course, Category, Episode };
