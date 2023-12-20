dea7181df0cd14c70a7c8e63bcd583f543c7f6c7
Juliano Almada <almadaj@edu.unifor.br>
2023-12-20 17:22:05 -0300

    tabela de usuários e encriptação de senhas

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,10 +1,11 @@
 import { Category } from "./Category";
 import { Course } from "./Course";
 import { Episode } from "./Episode";
+import { User } from "./User";
 
 Category.hasMany(Course);
 Course.belongsTo(Category);
 Course.hasMany(Episode);
 Episode.belongsTo(Course);
 
-export { Course, Category, Episode };
+export { Course, Category, Episode, User };
