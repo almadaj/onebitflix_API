9c2b3363cce4c3c36b8f8c549d46c6374fcecad1
Juliano Almada <almadaj@edu.unifor.br>
2023-12-21 17:09:25 -0300

    rota e paginação de category implementadas

---
+++ src/controllers/categoriesController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { categoryService } from "../services/categoryService";
+import { getPaginationParams } from "../helpers/getPaginationParams";
+export const categoriesController = {
+  index: async (req: Request, res: Response) => {
+    const [page, perPage] = getPaginationParams(req.query);
+
+    try {
+      const paginatedCategories = await categoryService.findAllPaginated(
+        page,
+        perPage
+      );
+
+      return res.json(paginatedCategories);
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message });
+      }
+    }
+  },
+};
---
+++ src/services/categoryService.ts
@@ -0,0 +1,20 @@
+import { Category } from "../models/Category";
+
+export const categoryService = {
+  findAllPaginated: async (page: number, perPage: number) => {
+    const offset = (page - 1) * perPage;
+
+    const { count, rows } = await Category.findAndCountAll({
+      attributes: ["id", "name", "position"],
+      order: [["position", "ASC"]],
+      limit: perPage,
+      offset,
+    });
+    return {
+      categories: rows,
+      page: page,
+      perPage: perPage,
+      total: count,
+    };
+  },
+};
