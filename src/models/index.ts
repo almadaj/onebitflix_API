1545d354ace87e134281925420fdb4780af00909
Juliano Almada <almadaj@edu.unifor.br>
2023-12-22 20:06:27 -0300

    criação do featured

--- src/models/index.ts
+++ src/models/index.ts
@@ -5,7 +5,7 @@ import { User } from "./User";
 
 Category.hasMany(Course, { as: "courses" });
 Course.belongsTo(Category);
-Course.hasMany(Episode);
+Course.hasMany(Episode, { as: "episodes" });
 Episode.belongsTo(Course);
 
 export { Course, Category, Episode, User };
