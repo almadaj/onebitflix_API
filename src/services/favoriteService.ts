1083f7bf6fe39d89e17ff094a5ded74dff30827b
Juliano Almada <almadaj@edu.unifor.br>
2023-12-27 11:11:27 -0300

    refatorando arquivos

--- src/services/favoriteService.ts
+++ src/services/favoriteService.ts
@@ -35,4 +35,13 @@ export const favoriteService = {
       },
     });
   },
+  isFavorited: async (userId: number, courseId: number) => {
+    const favorite = await Favorite.findOne({
+      where: {
+        userId,
+        courseId,
+      },
+    });
+    return favorite !== null;
+  },
 };
