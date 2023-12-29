e7543a7c211e0b7dfbce487e95732c266585f40d
Juliano Almada <almadaj@edu.unifor.br>
2023-12-29 11:12:51 -0300

    atualização de informações do usuário implementado

--- src/routes.ts
+++ src/routes.ts
@@ -42,6 +42,7 @@ router.post("/likes", ensureAuth, likesController.save);
 router.delete("/likes", ensureAuth, likesController.delete);
 
 router.get("/users/current", ensureAuth, usersController.show);
+router.put("/users/current", ensureAuth, usersController.update);
 router.get("/users/current/watching", ensureAuth, usersController.watching);
 
 export { router };
