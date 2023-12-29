015f7b21b175cf0f15291f4c1b785b1aa80f35d4
Juliano Almada <almadaj@edu.unifor.br>
2023-12-29 12:06:16 -0300

    instalação do cors e alterar senha implementado

--- src/routes.ts
+++ src/routes.ts
@@ -43,6 +43,11 @@ router.delete("/likes", ensureAuth, likesController.delete);
 
 router.get("/users/current", ensureAuth, usersController.show);
 router.put("/users/current", ensureAuth, usersController.update);
+router.put(
+  "/users/current/password",
+  ensureAuth,
+  usersController.updatePassword
+);
 router.get("/users/current/watching", ensureAuth, usersController.watching);
 
 export { router };
